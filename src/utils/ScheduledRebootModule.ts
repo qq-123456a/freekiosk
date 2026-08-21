01736cd79d1abb1dcedcc8cdbed88273bfb2d35a
roddys <qq123456a@gmail.com>
2026-08-21 14:24:49 +1000

    fix: expose exact alarm request support

--- src/utils/ScheduledRebootModule.ts
+++ src/utils/ScheduledRebootModule.ts
@@ -7,6 +7,7 @@ export interface ScheduledRebootSettings {
   nextTriggerAt: number;
   isDeviceOwner: boolean;
   exactAlarmAvailable: boolean;
+  exactAlarmRequestSupported: boolean;
 }
 
 interface ScheduledRebootModuleInterface {
